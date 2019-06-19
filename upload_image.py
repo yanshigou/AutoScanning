64f74f269868813ceb0f185e9fcf02e03df91b23
cmx_company <569578851@qq.com>
2019-06-19 18:30:08 +0800

    :fire::bug::ambulance::sparkles::checkered_flag::hammer: 重大重构 可配置化 新增车型判断、删除无用空文件夹 解决空文件夹下报错

--- upload_image.py
+++ upload_image.py
@@ -6,13 +6,15 @@ import requests
 import os
 import shutil
 from time import sleep
+from datetime import datetime
 
 
-def event(event_files_list, move_folder, ip_val):
+def event(event_files_list, move_folder, ip_val, event_path, now_time):
     count = 0
     try:
         for file in event_files_list:
             # 如果是文件,则打印
+            split_val = event_path.split("\\")[-1]
             if file.is_file:
                 if file.file_name[-3:] == 'jpg':
                     count += 1
@@ -21,9 +23,11 @@ def event(event_files_list, move_folder, ip_val):
                     # path_list = file_path.split('\\')
                     # path_list[4] = "备份"
                     # folder = "\\".join(path_list[:-1])
-                    print(file_path.split('事件'))
-                    path_list = file_path.split('事件')
-                    folder = move_folder + "\\事件" + path_list[-1]
+
+                    split_path_list = file_path.split(split_val)[-1]
+                    path_list = split_path_list.split("\\")[:-1]
+                    path_list_folder = "\\".join(path_list)
+                    folder = move_folder + ("\\%s" % split_val) + path_list_folder
 
                     print(folder)
 
@@ -39,7 +43,17 @@ def event(event_files_list, move_folder, ip_val):
                         # 进行对事件进行操作
                         ip = file_name_list[4]
                         car_id = file_name_list[5]
-                        car_type = file_name_list[6]
+
+                        car_color = file_name_list[-1][0]
+                        print(car_color)
+                        car_type = '00'
+                        if car_color == "蓝":
+                            car_type = '00'
+                        elif car_color == '黄':
+                            car_type = '01'
+                        elif car_color == '绿':
+                            car_type = '04'
+
                         wf_time = file_name_list[1] + file_name_list[2]
                         f = open(file_path, 'rb')
                         files = {'image_file': (file_name, f, 'image/jpg')}
@@ -54,74 +68,138 @@ def event(event_files_list, move_folder, ip_val):
                         print(status)
                         print(res['is_del'])
                         f.close()
-                        if res['is_del']:
+                        try:
+                            if res['is_del']:
+                                os.remove(file_path)
+                                print('删除成功')
+                            else:
+                                shutil.move(file_path, folder)
+                                print('移动成功')
+                        except Exception as ex:
                             os.remove(file_path)
                             print('删除成功')
-                        else:
-                            shutil.move(file_path, folder)
-                            print('移动成功')
-                        sleep(0.5)
-                        return {"status": "success", "count": count, "res_status": status, "file_path": file_path, "folder": folder}
+                        finally:
+                            sleep(0.1)
+                            return {"status": "success", "count": count, "res_status": status, "file_path": file_path, "folder": folder}
+            else:
+                try:
+                    folder_path = file.file_path
+                    print(folder_path)
+                    folder_val = folder_path.split('\\')[-1]
+                    folder_day = datetime.strftime(now_time, '%Y%m%d')
+                    if not os.listdir(folder_path) and (folder_day not in folder_path) and folder_val != split_val:
+                        print("空文件夹，删除")
+                        os.removedirs(folder_path)
+                except Exception as exc:
+                    print(exc)
+                    pass
+        return {"status": "over", "count": count}
     except Exception as e:
         print(e)
-        return {"status": "error", "e": e, "res_status": "fail", "count": count}
+        return {"status": "error", "e": e, "res_status": "error", "count": count}
 
 
-def QZ(files_list):
+def QZ(files_list, move_folder, ip_val, qz_path, now_time):
     count = 0
-    for file in files_list:
-        # 如果是文件,则打印
-        if file.is_file:
-            sleep(1)
-            if file.file_name[-3:] == 'jpg':
-                count += 1
-                file_path = file.file_path
-                path_list = file_path.split('\\')
-                path_list[4] = "备份"
-                folder = "\\".join(path_list[:-1])
-                print(folder)
+    # for path, dirs, files in os.walk(qz_path):
+    #     print("path", path)
+    #     print("dirs", dirs)
+    #     print("files", files)
+    #     if not dirs and not files:
+    #         print('空文件夹，删除')
+    #         os.rmdir(path)
+    try:
+        for file in files_list:
+            # 如果是文件,则打印
+            split_val = qz_path.split("\\")[-1]
+            if file.is_file:
+                if file.file_name[-3:] == 'jpg':
+                    count += 1
+                    file_path = file.file_path
 
-                if not os.path.exists(folder):
-                    os.makedirs(folder)
+                    # path_list = file_path.split('\\')
+                    # path_list[4] = "备份"
+                    # folder = "\\".join(path_list[:-1])
 
-                file_path = file.file_path
-                file_name = file.file_name
-                file_name_list = file_name.split('_')
-                print(file_name_list)
-                i_type = file_name_list[0]
-                if i_type == 1039 or 10396:
-                    print("取证图片")
-                    # 对取证进行操作
-                    ip = file_name_list[4]
-                    car_id = file_name_list[5]
-                    car_type = file_name_list[6]
-                    wf_time = file_name_list[1]+file_name_list[2]
-                    f = open(file_path, 'rb')
-                    files = {'image_file': (file_name, f, 'image/jpg')}
+                    split_path_list = file_path.split(split_val)[-1]
+                    path_list = split_path_list.split("\\")[:-1]
+                    path_list_folder = "\\".join(path_list)
+                    folder = move_folder + ("\\%s" % split_val) + path_list_folder
 
-                    data = dict(
-                        data_type='WT', ip=ip, car_id=car_id, car_type=car_type, wf_time=wf_time
-                    )
+                    print(folder)
 
-                    res = requests.post('http://127.0.0.1:8000/dataInfo/wtDataInfoUpload/', files=files, data=data).json()
-                    status = res['status']
-                    print(status)
-                    print(res['is_del'])
-                    f.close()
-                    if res['is_del']:
-                        os.remove(file_path)
-                        print('删除成功')
-                    else:
+                    if not os.path.exists(folder):
+                        os.makedirs(folder)
+
+                    file_path = file.file_path
+                    file_name = file.file_name
+                    file_name_list = file_name.split('_')
+                    print(file_name_list)
+                    i_type = file_name_list[0]
+                    print(i_type)
+                    print(type(i_type))
+                    if i_type == ('1039' or "10396"):
+
+                        print("取证图片")
+                        # 对取证进行操作
+                        ip = file_name_list[4]
+                        car_id = file_name_list[5]
+                        car_color = file_name_list[-1][0]
+                        print(car_color)
+                        car_type = '00'
+                        if car_color == "蓝":
+                            car_type = '00'
+                        elif car_color == '黄':
+                            car_type = '01'
+                        elif car_color == '绿':
+                            car_type = '04'
+                        wf_time = file_name_list[1]+file_name_list[2]
+                        f = open(file_path, 'rb')
+                        files = {'image_file': (file_name, f, 'image/jpg')}
+
+                        data = dict(
+                            data_type='WT', ip=ip, car_id=car_id, car_type=car_type, wf_time=wf_time
+                        )
+
+                        res = requests.post('http://%s:8000/dataInfo/wtDataInfoUpload/' % ip_val, files=files, data=data).json()
+                        status = res['status']
+                        print(status)
+                        print(res['is_del'])
+                        f.close()
                         try:
-                            shutil.move(file_path, folder)
-                            print('移动成功')
-                        except Exception as e:
-                            print(e)
+                            if res['is_del']:
+                                os.remove(file_path)
+                                print('删除成功')
+                            else:
+                                shutil.move(file_path, folder)
+                                print('移动成功')
+                        except Exception as ex:
                             os.remove(file_path)
                             print('删除成功')
-    return count
+                        finally:
+                            sleep(0.5)
+                            return {"status": "success", "count": count, "res_status": status, "file_path": file_path, "folder": folder}
+            else:
+                try:
+                    folder_path = file.file_path
+                    print(folder_path)
+                    folder_val = folder_path.split('\\')[-1]
+                    folder_day = datetime.strftime(now_time, '%Y%m%d')
+                    if not os.listdir(folder_path) and (folder_day not in folder_path) and folder_val != split_val:
+                        print("空文件夹，删除")
+                        os.removedirs(folder_path)
+                except Exception as exc:
+                    print(exc)
+                    pass
+        return {"status": "over", "count": count}
+    except Exception as e:
+        print(e)
+        return {"status": "error", "e": e, "res_status": "error", "count": count}
 
 
 if __name__ == '__main__':
-    a = event(FileObjectManager(FileObject("G:\dzt\资料\交警\测试文件夹\事件")).scan_with_depth(10).all_file_objects(), "G:\dzt\资料\交警\备份", '192.168.31.54')
+    a = event(FileObjectManager(FileObject("G:\dzt\资料\交警\测试文件夹\事件")).scan_with_depth(10).all_file_objects(), "G:\dzt\资料\交警\备份", '192.168.31.54', "G:\dzt\资料\交警\测试文件夹\事件", datetime.now())
     print(a)
+
+    b = QZ(FileObjectManager(FileObject("G:\dzt\资料\交警\测试文件夹\取证")).scan_with_depth(10).all_file_objects(), "G:\dzt\资料\交警\备份", '192.168.31.54', "G:\dzt\资料\交警\测试文件夹\取证", datetime.now())
+    print(b)
