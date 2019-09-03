1fdb67ddf79a74ba5f5d957bb5fea475c9009a8f
cmx_company <569578851@qq.com>
2019-09-03 14:39:46 +0800

    修改不显示cmd窗口

--- deviceStatus.py
+++ deviceStatus.py
@@ -2,7 +2,7 @@ __author__ = "dzt"
 __date__ = "2019/8/2"
 
 from platform import system as system_name
-from os import system
+from os import system, popen
 import requests
 
 
@@ -11,7 +11,7 @@ def ping(host):
     # 判断系统
     parameters = "-n 1" if system_name().lower() == "windows" else "-c 1"
 
-    return system("ping " + parameters + " " + host) == 0
+    return popen("ping " + parameters + " " + host) == 0
 
 
 if __name__ == '__main__':
