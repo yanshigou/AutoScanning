a9d449f945257fb005be1d01adeba373568852df
cmx_company <569578851@qq.com>
2019-10-23 13:42:04 +0800

    默认0

--- dj/AutoScanning/upload_image.py
+++ dj/AutoScanning/upload_image.py
@@ -12,52 +12,46 @@ dj_url = "/djDataInfo/djDataInfoUpload/"
 
 
 def QZ(files_list, move_folder, ip_val, qz_path, now_time, white_list, wf_list):
-    print(wf_list)
+    # print(wf_list)
     try:
         for file in files_list:
             # 如果是文件,则打印
             split_val = qz_path.split("\\")[-1]
-            print(split_val)
+            # print(split_val)
             if file.is_file:
                 if file.file_name[-3:] == 'jpg':
                     file_path = file.file_path
                     file_name = file.file_name
                     file_name_list = file_name.split('_')
-                    print("file_path", file_path)
-                    print("file_name", file_name)
-                    print("file_name_list", file_name_list)
+                    # print("file_path", file_path)
+                    # print("file_name", file_name)
+                    # print("file_name_list", file_name_list)
                     i_type = file_name_list[0]
-                    print(i_type)
-                    print(type(i_type))
+                    # print(i_type)
+                    # print(type(i_type))
                     try:
                         car_id = file_name_list[5]
-                        print(car_id)
+                        # print(car_id)
                     except Exception as e:
                         print(e)
                         os.remove(file_path)
                         continue
                     if '无' in car_id:
-                        print(car_id)
+                        # print(car_id)
                         os.remove(file_path)
                         continue
                     if i_type in wf_list:
-                        print("取证图片")
                         # 对取证进行操作
                         ip = file_name_list[4]
                         if car_id in white_list:
                             os.remove(file_path)
-                            print('在白名单内，删除成功')
                             continue
                         wf_time = file_name_list[1] + file_name_list[2]
                         car_color = file_name_list[-1][0]
-                        print(car_color)
-                        car_type = '00'
-                        if car_color == "蓝":
-                            car_type = '02'
-                        elif car_color == '黄':
+                        # print(car_color)
+                        car_type = '02'
+                        if car_color == '黄':
                             car_type = '01'
-                        elif car_color == '绿':
-                            car_type = '02'
                         try:
                             f = open(file_path, 'rb')
                             files = {'image_file': (file_name, f, 'image/jpg')}
@@ -76,8 +70,7 @@ def QZ(files_list, move_folder, ip_val, qz_path, now_time, white_list, wf_list):
                             continue
 
                         status = res['status']
-                        print("status=" + status)
-                        print("is_del=" + str(res['is_del']))
+                        # print("status=" + status)
                         count = 0
                         # 只有删除才去使用移动后的路径，
                         try:
@@ -95,13 +88,13 @@ def QZ(files_list, move_folder, ip_val, qz_path, now_time, white_list, wf_list):
             else:
                 try:
                     folder_path = file.file_path
-                    print(folder_path)
+                    # print(folder_path)
                     folder_val = folder_path.split('\\')[-1]
-                    print(folder_val)
+                    # print(folder_val)
                     folder_day = datetime.strftime(now_time, '%Y%m%d')
                     if not os.listdir(folder_path) and (folder_day not in folder_path) and folder_val != split_val and (
                             '.' not in folder_val):
-                        print("空文件夹，删除")
+                        # print("空文件夹，删除")
                         os.rmdir(folder_path)
                 except Exception as exc:
                     print(exc)
@@ -114,8 +107,8 @@ def QZ(files_list, move_folder, ip_val, qz_path, now_time, white_list, wf_list):
 
 
 if __name__ == '__main__':
-    wf_list = ['13443', '10395']
-    file_list = FileObjectManager(FileObject(r"D:\WorkDocument\ITS\quzheng\电警")).scan_with_depth(10).all_file_objects()
-    b = QZ(file_list, r"D:\WorkDocument\ITS\backup", r'192.168.31.54:8000', r"D:\WorkDocument\ITS\quzheng\电警",
+    wf_list = ['13443', '10395', '13442']
+    file_list = FileObjectManager(FileObject("G:\dzt\资料\交警\载货汽车限行")).scan_with_depth(10).all_file_objects()
+    b = QZ(file_list, r"D:\WorkDocument\ITS\backup", r'192.168.31.54:8000', "G:\dzt\资料\交警\载货汽车限行",
            datetime.now(), [], wf_list)
     print(b)
