acf106f8b06a51d8c03556f6844b0fa7e8113227
yanshigoudeMacBook <569578851@qq.com>
2022-08-07 18:01:53 +0800

    违停扫描器新

---
+++ wt/wt_main.py
@@ -0,0 +1,303 @@
+# -*- coding: utf-8 -*-
+__author__ = "dzt"
+__date__ = "2022/8/6"
+__title__ = "电警扫描器v0.1.0_20220807"
+
+from wt.file_manager import FileObjectManager, FileObject
+import requests
+import tkinter as tk
+import threading
+from tkinter import scrolledtext
+from wt.wt_upload_image import event, QZ
+from datetime import datetime
+from time import sleep
+import configparser
+
+# 建立连接 获取csrftoken
+client = requests.session()
+event_thread_count = 0
+qz_thread_count = 0
+
+
+def basic_info():
+    white_list = ['渝DJD020']
+    try:
+        cf = configparser.ConfigParser()
+        cf.read("ACconfig.ini", encoding="utf-8-sig")  # 读取配置文件，如果写文件的绝对路径，就可以不用os模块
+
+
+        # options = cf.options("DJConfig")  # 获取某个section名为BasicConfig所对应的键
+        # print(options)
+        #
+        # items = cf.items("DJConfig")  # 获取section名为BasicConfig所对应的全部键值对
+        # print(items)
+
+        qz_path = cf.get("WTConfig", "qz_path")  # 获取[BasicConfig]中qz_path对应的值
+
+        event_path = cf.get("WTConfig", "event_path")  # 获取[BasicConfig]中qz_path对应的值
+
+        ip_val = cf.get("WTConfig", "ip_val")  # 获取[BasicConfig]中ip_val对应的值
+
+        sleep_time = cf.get("WTConfig", "sleep_time")  # 获取[BasicConfig]中sleep_time对应的值
+
+        qz_time = cf.get("WTConfig", "qz_time")  # 获取[BasicConfig]中qz_time对应的值
+
+        wf_list_str = cf.get("WTConfig", "wf_list")  # 获取[BasicConfig]中wf_list对应的值
+        wf_list = wf_list_str.split(',')
+
+        return event_path, qz_path, ip_val, white_list, sleep_time, qz_time, wf_list
+    except Exception as e:
+        print(e)
+        return False
+
+
+def event_run(event_path, ip_val, white_list, sleep_time):
+    log_file = datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
+    f = open(log_file, 'a+', encoding='utf-8')
+    text.insert(tk.END, "开始扫描事件文件夹：%s\n" % event_path)
+    f.write("开始扫描事件文件夹：%s\n" % event_path)
+    f.close()
+    global event_thread_count
+    event_thread_count += 1
+    # event_bt.config(state="disabled", text='正在扫描事件文件夹')
+    count_event_thread_lb.config(text="\n已开启事件扫描器数：%s\n" % str(event_thread_count))
+    while True:
+        log_file = datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
+        try:
+            f = open(log_file, 'a+', encoding='utf-8')
+            now_time = datetime.now()
+            res = event(FileObjectManager(FileObject(event_path)).scan_with_depth(10).all_file_objects(),
+                        ip_val, event_path, now_time, white_list)
+            # print(res)
+            status = res.get('status')
+            res_status = res.get('res_status')
+            e = res.get('e')
+            file_path = res.get('file_path')
+
+            zpname = res.get('zpname')
+            zp = res.get('zp')
+
+            now_time = datetime.now()
+            if zpname:
+                text.insert(tk.END, '\n%s 短信发送成功，已收到短信图片%s，等待扫描上传' % (now_time, zpname))
+                f.write('\n%s 短信发送成功，已收到短信图片%s，等待扫描上传' % (now_time, zpname))
+            if status == "success" and zp != 'success':
+
+                if res_status == "success":
+                    res_status = '成功'
+                elif res_status == "error":
+                    res_status = '出错'
+                    text.insert(tk.END, "\n%s 【事件】【扫描到文件】%s，服务器【%s】，%s" % (now_time, file_path, res_status, e))
+                    f.write("\n%s 【事件】【扫描到文件】%s，服务器【%s】，%s" % (now_time, file_path, res_status, e))
+                else:
+                    res_status = '失败'
+                text.insert(tk.END, "\n%s 【事件】【扫描到文件】%s，上传文件【%s】\n" % (now_time, file_path, res_status))
+                f.write("\n%s 【事件】【扫描到文件】%s，上传文件【%s】\n" % (now_time, file_path, res_status))
+            elif status == "success" and zp == 'success':
+                if res_status == "success":
+                    res_status = '成功'
+                elif res_status == "error":
+                    res_status = '出错'
+                    text.insert(tk.END, "\n%s 【事件-短信】【扫描到文件】%s，服务器【%s】，%s" % (now_time, file_path, res_status, e))
+                    f.write("\n%s 【事件-短信】【扫描到文件】%s，服务器【%s】，%s" % (now_time, file_path, res_status, e))
+                else:
+                    res_status = '失败'
+                text.insert(tk.END,
+                            "\n%s 【事件-短信】【扫描到文件】%s，上传文件【%s】\n" % (now_time, file_path, res_status))
+                f.write("\n%s 【事件-短信】【扫描到文件】%s，上传文件【%s】\n" % (now_time, file_path, res_status))
+            elif status == "fail":
+                text.insert(tk.END, "\n%s 【事件】【扫描到文件夹】 %s，【未发现图片】\n" % (now_time, event_path))
+                f.write("\n%s 【事件】【扫描到文件夹】 %s，【未发现图片】" % (now_time, event_path))
+            elif status == "error":
+                text.insert(tk.END, "\n%s 【事件】【上传图片出错，请查看日志】：%s\n" % (now_time, e))
+                f.write("\n%s 【事件】【上传图片出错，请检查日志】：%s" % (now_time, e))
+            elif status == "over":
+                text.insert(tk.END, "\n%s 【事件】【扫描到文件夹】 %s，【未发现任何图片，休息%s秒】\n" % (now_time, event_path, sleep_time.strip()))
+                f.write("\n%s 【事件】【扫描到文件夹】 %s，【未发现任何图片，休息%s秒】\n" % (now_time, event_path, sleep_time.strip()))
+                text.see(tk.END)
+                sleep(int(sleep_time))
+            text.see(tk.END)
+            # sleep(0.1)
+        except Exception as ee:
+            text.insert(tk.END, "\n%s 【事件】程序出错：%s\n" % (datetime.now(), str(ee)))
+            text.see(tk.END)
+            f.write("\n%s 【事件】程序出错：%s" % (datetime.now(), str(ee)))
+        finally:
+            f.close()
+
+
+def QZ_run(qz_path, ip_val, white_list, sleep_time, qz_time, wf_list):
+    log_file = datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
+    f = open(log_file, 'a+', encoding='utf-8')
+    text.insert(tk.END, "\n开始扫描取证文件夹：%s\n" % qz_path)
+    f.write("%s \n开始扫描取证文件夹：%s" % (qz_path, datetime.now()))
+    f.close()
+    global qz_thread_count
+    qz_thread_count += 1
+    # qz_bt.config(state="disabled", text='正在扫描取证文件夹')
+    count_qz_thread_lb.config(text="\n已开启取证扫描器数：%s\n" % str(qz_thread_count))
+    while True:
+        log_file = datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
+        try:
+            f = open(log_file, 'a+', encoding='utf-8')
+            now_time = datetime.now()
+            res = QZ(FileObjectManager(FileObject(qz_path)).scan_with_depth(10).all_file_objects(),
+                     ip_val, qz_path, now_time, white_list, wf_list)
+            status = res.get('status')
+            res_status = res.get('res_status')
+            e = res.get('e')
+            file_path = res.get('file_path')
+
+            if status == "success":
+                if res_status == "success":
+                    res_status = '成功'
+                    text.insert(tk.END,
+                                "\n%s【取证】【扫描到文件】%s，上传文件【%s】\n" % (now_time, file_path, res_status))
+                    f.write("\n%s 【取证】【扫描到文件】%s，上传文件【%s】\n" % (now_time, file_path, res_status))
+                elif res_status == "error":
+                    res_status = '出错'
+                    text.insert(tk.END, "\n%s 【取证】【扫描到文件】%s，服务器【%s】，%s" % (now_time, file_path, res_status, e))
+                    f.write("\n%s 【取证】【扫描到文件】%s，服务器【%s】，%s" % (now_time, file_path, res_status, e))
+                elif res_status == "wait":
+                    res_status = '再次推送'
+                    text.insert(tk.END,
+                                "\n%s 【取证】【扫描到文件】%s，已上传取证图片，未推送成功，【%s】，%s" % (now_time, file_path, res_status, e))
+                    f.write("\n%s 【取证】【扫描到文件】%s，已上传取证图片，未推送成功，【%s】，%s" % (now_time, file_path, res_status, e))
+                else:
+                    res_status = '失败'
+                    text.insert(tk.END,
+                                "\n%s【取证】【扫描到文件】%s，上传文件【%s】, 该设备未启用\n" % (now_time, file_path, res_status))
+                    f.write("\n%s 【取证】【扫描到文件】%s，上传文件【%s】, 该设备未启用\n" % (now_time, file_path, res_status))
+            elif status == "fail":
+                text.insert(tk.END, "\n%s【取证】【扫描到文件夹】 %s，【未发现图片】\n" % (now_time, qz_path))
+                f.write("\n%s 【取证】【扫描到文件夹】 %s，【未发现图片】" % (now_time, qz_path))
+            elif status == "error":
+                text.insert(tk.END, "\n%s【取证】【上传出错，请查看日志】：%s\n" % (now_time, e))
+                f.write("\n%s 【取证】【上传出错，请检查日志】：%s" % (now_time, e))
+            elif status == "over":
+                text.insert(tk.END, "\n%s 【取证】【扫描到文件夹】 %s，【未发现任何图片，休息%s秒】\n" % (now_time, qz_path, sleep_time.strip()))
+                f.write("\n%s 【取证】【扫描到文件夹】 %s，【未发现任何图片，休息%s秒】\n" % (now_time, qz_path, sleep_time.strip()))
+                text.see(tk.END)
+                sleep(int(sleep_time))
+            text.see(tk.END)
+            # 大量同时推送集成平台时，会出错
+            if qz_time != '0':
+                sleep(int(qz_time))
+        except Exception as ee:
+            text.insert(tk.END, "\n%s 【取证】程序出错：%s\n" % (datetime.now(), str(ee)))
+            text.see(tk.END)
+            f.write("\n%s 【取证】程序出错：%s" % (datetime.now(), str(ee)))
+        finally:
+            f.close()
+
+
+def thread_it(func, *args):
+    """
+    将函数打包进线程
+    :param func:
+    :param args:
+    :return:
+    """
+    # 创建
+    t = threading.Thread(target=func, args=args)
+    # 守护 !!!
+    t.setDaemon(True)
+    # 启动
+    t.start()
+    # 阻塞--卡死界面！
+    # t.join()
+
+
+def auto_run(event_path, ip_val, white_list, sleep_time, qz_time):
+    log_file = datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
+    f = open(log_file, 'a+', encoding='utf-8')
+    try:
+        text.insert(tk.END, "%s 5秒后自动开始运行 事件扫描 \n" % datetime.now())
+        text.insert(tk.END, "%s 30秒后自动开始运行 取证扫描 \n" % datetime.now())
+        f.write("%s 5秒后自动开始运行 事件扫描 \n" % datetime.now())
+        f.write("%s 30秒后自动开始运行 取证扫描  \n" % datetime.now())
+        sleep(5)
+        thread_it(event_run, event_path, ip_val, white_list, sleep_time)
+        sleep(25)
+        thread_it(QZ_run, qz_path, ip_val, white_list, sleep_time, qz_time, wf_list)
+    except Exception as e:
+        print(e)
+        f.write("%s 自动运行出错 %s\n" % (datetime.now(), str(e)))
+        text.insert(tk.END, "%s 自动运行出错 %s\n" % (datetime.now(), str(e)))
+    finally:
+        f.close()
+
+
+if __name__ == '__main__':
+
+    log_file = datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
+    f = open(log_file, 'a+', encoding='utf-8')
+    f.write('%s 正在读取配置文件...\n' % datetime.now())
+    f.close()
+
+    if basic_info():
+        event_path, qz_path, ip_val, white_list, sleep_time, qz_time, wf_list = basic_info()
+
+        root = tk.Tk()
+
+        root.title(__title__)
+
+        # 滚动条
+        scroll = tk.Scrollbar()
+
+        text = scrolledtext.ScrolledText(root, width=90, height=60)
+
+        text.insert(tk.END, "配置信息如下：\n")
+        text.insert(tk.END, "事件文件夹路径：%s\n" % event_path)
+        text.insert(tk.END, "取证文件夹路径：%s\n" % qz_path)
+        text.insert(tk.END, "服务器及端口：%s\n" % ip_val)
+        text.insert(tk.END, "空闲间隔(秒)：%s\n" % sleep_time)
+        text.insert(tk.END, "取证图片上传间隔(秒)：%s\n" % qz_time)
+        # text.insert(tk.END, "白名单：%s\n" % white_list)
+
+        text.grid(row=0, column=0)
+        root.geometry('1000x800+600+50')
+        lf = tk.LabelFrame(root, text='')
+        lf.grid(row=0, column=1)
+
+        # event_lb = tk.Label(lf, text="事件文件夹路径为：")
+        # event_lb.grid()
+
+        # event_val = tk.Label(lf, text=event_path, wraplength=400)
+        # event_val.grid()
+
+        # count_event_lb = tk.Label(lf, text="\n已处理事件图片数：0\n")
+        # count_event_lb.grid()
+        count_event_thread_lb = tk.Label(lf, text="\n已开启事件扫描器数：0\n")
+        count_event_thread_lb.grid()
+
+        # sms_lb = tk.Label(lf, text="已处理短信图片数：0\n")
+        # sms_lb.grid()
+
+        # qz_lb = tk.Label(lf, text="取证文件夹路径为：")
+        # qz_lb.grid()
+
+        # qz_val = tk.Label(lf, text=qz_path, wraplength=400)
+        # qz_val.grid()
+
+        # count_qz_lb = tk.Label(lf, text="\n已处理取证图片数：0\n")
+        # count_qz_lb.grid()
+        count_qz_thread_lb = tk.Label(lf, text="\n已开启取证扫描器数：0\n")
+        count_qz_thread_lb.grid()
+
+        event_bt = tk.Button(lf, text='开始扫描事件文件夹', fg='red',
+                             command=lambda: thread_it(event_run, event_path, ip_val, white_list,
+                                                       sleep_time))
+        qz_bt = tk.Button(lf, text='开始扫描取证文件夹', fg='red',
+                          command=lambda: thread_it(QZ_run, qz_path, ip_val, white_list, sleep_time,
+                                                    qz_time, wf_list))
+        event_bt.grid(padx=5, pady=20)
+        qz_bt.grid(padx=5, pady=20)
+
+        thread_it(auto_run, event_path, ip_val, white_list, sleep_time, qz_time)
+
+        q = tk.Button(lf, text='退  出', command=root.quit, padx=10, pady=5)
+        q.grid(padx=5, pady=10)
+
+        root.mainloop()
+
---
+++ wt/wt_upload_image.py
@@ -0,0 +1,323 @@
+# -*- coding: utf-8 -*-
+__author__ = "dzt"
+__date__ = "2019/6/18"
+from wt.file_manager import FileObjectManager, FileObject
+import requests
+import os
+import re
+from datetime import datetime
+import base64
+import random
+
+
+def event(event_files_list, ip_val, event_path, now_time, white_list):
+    log_file = datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
+    flog = open(log_file, 'a+', encoding='utf-8')
+    try:
+        if event_files_list:
+            file = random.choice(event_files_list)
+        else:
+            return {"status": "over", "count": 0}
+        # 如果是文件,则打印
+        split_val = event_path.split("\\")[-1]
+        if file.is_file:
+            if file.file_name[-3:] == 'jpg':
+                file_path = file.file_path
+                file_name = file.file_name
+                file_name_list = file_name.split('_')
+                image_folder = "\\".join(file_path.split("\\")[:-1]) + '\\'
+
+                i_type = file_name_list[0]
+                last_str = file_name_list[-1]
+                car_id = ""
+                ip = ""
+                car_color = ""
+                time1 = file_name_list[1]
+                time2 = file_name_list[2]
+
+                for i in file_name_list:
+                    re_car_id = re.findall('^[无京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z].*', i, re.S)
+                    if re_car_id:
+                        car_id = i
+
+                    re_time1 = re.findall('^20\d{6}$', i, re.S)
+                    if re_time1:
+                        time1 = i
+                    re_time2 = re.findall('^\d{6}$', i, re.S)
+                    if re_time2:
+                        time2 = i
+
+                    re_ip = re.findall('^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', i, re.S)
+                    if re_ip:
+                        ip = i
+
+                    re_car_color = re.findall('^.*\.jpg$', i, re.S)
+                    if re_car_color:
+                        car_color = i
+                wf_time = time1 + time2
+                if '无' in car_id:
+                    flog.write("\n%s 【事件扫描删除】【无车牌】%s\n" % (now_time, file_path))
+                    os.remove(file_path)
+                    return {"status": "fail", "count": 0, "res_status": "error", "file_path": file_path}
+                if i_type == '2' and ('短信' not in last_str):
+
+                    if car_id in white_list:
+                        os.remove(file_path)
+                        return {"status": "fail", "count": 0, "res_status": "error", "file_path": file_path}
+                    car_type = '02'
+                    if car_color == '黄':
+                        car_type = '01'
+                    elif car_color == "绿":
+                        car_type = "52"
+                    elif "渐" in car_color:  # 渐变绿
+                        car_type = "51"
+
+                    try:
+                        f = open(file_path, 'rb')
+                        files = {'image_file': (file_name, f, 'image/jpg')}
+                        data = dict(
+                            data_type='WT', ip=ip, car_id=car_id, car_type=car_type,
+                            wf_time=wf_time
+                        )
+                        try:
+                            res = requests.post('http://%s/dataInfo/wtDataEventUpload/' % ip_val, files=files,
+                                                data=data).json()
+                            status = res['status']
+                        except Exception as e:
+                            flog.write("\n%s 【事件扫描上传出错】【%s】\n" % (now_time, e))
+                            # continue
+                            status = "error"
+                            res = {"zp": "", "sms_image": ""}
+                        finally:
+                            f.close()
+                    except Exception as e:
+                        flog.write("\n%s 【事件扫描上传出错】【%s】\n" % (now_time, e))
+                        # continue
+                        status = "error"
+                        res = {"zp": "", "sms_image": ""}
+
+                    zp = res.get('zp')
+                    image_file = res.get('sms_image')
+                    zpname = ""
+                    if zp:
+                        # 存在本机后再传图片至服务器
+                        imgdata = base64.b64decode(zp)
+                        l = image_file.split('.jpg')
+                        l_name = l[0].split('_')
+                        l_name[0] = '1039'
+                        dx_name = '_'.join(l_name)
+                        zpname = image_folder + dx_name + '_短信' + '.jpg'
+                        img = open(zpname, 'wb')
+                        img.write(imgdata)
+                        img.close()
+                    count = 0
+                    try:
+                        os.remove(file_path)
+                        count = 1
+                    except Exception as ex:
+                        flog.write("\n%s 【事件扫描删除或移动出错】【%s】\n" % (now_time, ex))
+                    finally:
+
+                        return {
+                            "status": "success", "count": count, "sms_count": 0, "res_status": status, "file_path": file_path,
+                            "zpname": zpname, "e": res.get('e')}
+                        # return {
+                        #     "status": "success", "count": count, "sms_count": 0, "res_status": status, "file_path": file_path,
+                        #     "zpname": "", "e": ""}
+                elif '短信' in last_str:
+                    if car_id in white_list:
+                        os.remove(file_path)
+                        return {"status": "fail", "count": 0, "res_status": "error", "file_path": file_path}
+                    # car_color = file_name_list[-2][0]
+                    car_type = '02'
+                    if car_color == '黄':
+                        car_type = '01'
+                    elif car_color == "绿":
+                        car_type = "52"
+                    elif "渐" in car_color:  # 渐变绿
+                        car_type = "51"
+                    try:
+                        f = open(file_path, 'rb')
+                        files = {'image_file': (file_name, f, 'image/jpg')}
+                        data = dict(
+                            data_type='WT', ip=ip, car_id=car_id, car_type=car_type,
+                            wf_time=wf_time
+                        )
+                        try:
+                            res = requests.post('http://%s/dataInfo/wtDataEventSMSUpload/' % ip_val, files=files,
+                                                data=data).json()
+                            status = res['status']
+                        except Exception as e:
+                            flog.write("\n%s 【事件扫描上传出错】【%s】\n" % (now_time, e))
+                            status = "error"
+                        finally:
+                            f.close()
+                    except Exception as e:
+                        flog.write("\n%s 【事件扫描上传出错】【%s】\n" % (now_time, e))
+                        status = "error"
+
+                    sms_count = 0
+                    try:
+                        # 直接删除不进行备份
+                        os.remove(file_path)
+                        sms_count = 1
+                    except Exception as ex:
+                        flog.write("\n%s 【事件扫描删除或移动出错】【%s】\n" % (now_time, ex))
+                    finally:
+                        return {"status": "success", "count": 0, "sms_count": sms_count, "res_status": status, "file_path": file_path,
+                                "zp": "success"}
+                else:
+                    os.remove(file_path)
+                    flog.write("\n%s 【事件扫描删除】【未在违法代码列表中】%s\n" % (now_time, file_path))
+                    return {"status": "fail", "count": 0, "res_status": "error", "file_path": file_path}
+            else:
+                file_path = file.file_path
+                flog.write("\n%s 【事件扫描删除】【非jpg文件】%s\n" % (now_time, file_path))
+                os.remove(file_path)
+                # continue
+                return {"status": "fail", "count": 0, "res_status": "error", "file_path": file_path}
+        else:
+            try:
+                folder_path = file.file_path
+                folder_val = folder_path.split('\\')[-1]
+                folder_day = datetime.strftime(now_time, '%Y%m%d')
+                if not os.listdir(folder_path) and (folder_day not in folder_path) and folder_val != split_val and ('.' not in folder_val):
+                    os.rmdir(folder_path)
+            except Exception as exc:
+                folder_path = file.file_path
+                flog.write("\n%s 【事件扫描删除空文件夹出错】【%s】\n" % (now_time, exc))
+                return {"status": "fail", "count": 0, "res_status": "error", "file_path": folder_path}
+        return {"status": "over", "count": 0, "sms_count": 0}
+    except Exception as e:
+        return {"status": "error", "e": str(e), "res_status": "error", "count": 0, "sms_count": 0}
+    finally:
+        flog.close()
+
+
+def QZ(files_list, ip_val, qz_path, now_time, white_list, wf_list):
+    log_file = datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
+    flog = open(log_file, 'a+', encoding='utf-8')
+    try:
+        if files_list:
+            file = random.choice(files_list)
+        else:
+            return {"status": "over", "count": 0}
+        # 如果是文件,则打印
+        split_val = qz_path.split("\\")[-1]
+        if file.is_file:
+            if file.file_name[-3:] == 'jpg':
+                file_path = file.file_path
+                file_name = file.file_name
+                file_name_list = file_name.split('_')
+
+                i_type = file_name_list[0]
+                car_id = ""
+                ip = ""
+                car_color = ""
+                time1 = file_name_list[1]
+                time2 = file_name_list[2]
+
+                for i in file_name_list:
+                    re_car_id = re.findall('^[无京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z].*', i, re.S)
+                    if re_car_id:
+                        car_id = i
+
+                    re_time1 = re.findall('^20\d{6}$', i, re.S)
+                    if re_time1:
+                        time1 = i
+                    re_time2 = re.findall('^\d{6}$', i, re.S)
+                    if re_time2:
+                        time2 = i
+
+                    re_ip = re.findall('^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', i, re.S)
+                    if re_ip:
+                        ip = i
+
+                    re_car_color = re.findall('^.*\.jpg$', i, re.S)
+                    if re_car_color:
+                        car_color = i
+                wf_time = time1 + time2
+                if '无' in car_id:
+                    flog.write("\n%s 【取证扫描删除】【无车牌】%s\n" % (now_time, file_path))
+                    os.remove(file_path)
+                    return {"status": "fail", "count": 0, "res_status": "error", "file_path": file_path}
+
+                if i_type in wf_list:
+
+                    # 对取证进行操作
+                    if car_id in white_list:
+                        os.remove(file_path)
+                        return {"status": "fail", "count": 0, "res_status": "error", "file_path": file_path}
+                    car_type = '02'
+                    if car_color == '黄':
+                        car_type = '01'
+                    elif car_color == "绿":
+                        car_type = "52"
+                    elif "渐" in car_color:  # 渐变绿
+                        car_type = "51"
+
+                    try:
+                        f = open(file_path, 'rb')
+                        files = {'image_file': (file_name, f, 'image/jpg')}
+                        data = dict(
+                            data_type='WT', ip=ip, car_id=car_id, car_type=car_type,
+                            wf_time=wf_time
+                        )
+                        try:
+                            res = requests.post('http://%s/dataInfo/wtDataInfoUpload/' % ip_val, files=files,
+                                                data=data).json()
+                            status = res['status']
+                        except Exception as e:
+                            flog.write("\n%s 【取证扫描上传出错】【%s】\n" % (now_time, e))
+                            # continue
+                            status = "error"
+                        finally:
+                            f.close()
+                    except Exception as e:
+                        flog.write("\n%s 【取证扫描上传出错】【%s】\n" % (now_time, e))
+                        # continue
+                        status = "error"
+
+                    count = 0
+                    try:
+
+                        # 直接删除不进行备份
+                        os.remove(file_path)
+                        count = 1
+                    except Exception as ex:
+                        flog.write("\n%s 【取证扫描删除或移动出错】【%s】\n" % (now_time, ex))
+                    finally:
+                        return {"status": "success", "count": count, "res_status": status, "file_path": file_path}
+                else:
+                    flog.write("\n%s 【取证扫描删除】【未在违法代码列表中】%s\n" % (now_time, file_path))
+                    os.remove(file_path)
+            else:
+                file_path = file.file_path
+                flog.write("\n%s 【取证扫描删除】【非jpg文件】%s\n" % (now_time, file_path))
+                os.remove(file_path)
+                # continue
+                return {"status": "fail", "count": 0, "res_status": "error", "file_path": file_path}
+        else:
+            try:
+                folder_path = file.file_path
+                folder_val = folder_path.split('\\')[-1]
+                folder_day = datetime.strftime(now_time, '%Y%m%d')
+                if not os.listdir(folder_path) and (folder_day not in folder_path) and folder_val != split_val and ('.' not in folder_val):
+                    os.rmdir(folder_path)
+            except Exception as exc:
+                folder_path = file.file_path
+                flog.write("\n%s 【取证扫描删除空文件夹出错】【%s】\n" % (now_time, exc))
+                return {"status": "fail", "count": 0, "res_status": "error", "file_path": folder_path}
+        return {"status": "over", "count": 0}
+    except Exception as e:
+        return {"status": "error", "e": str(e), "res_status": "error", "count": 0}
+    finally:
+        flog.close()
+
+
+if __name__ == '__main__':
+    a = event(FileObjectManager(FileObject("/Users/yanshigou/Downloads/15")).scan_with_depth(10).all_file_objects(), '0.0.0.0:8000', "/Users/yanshigou/Downloads/15", datetime.now(), [])
+    print(a)
+
+    b = QZ(FileObjectManager(FileObject("/Users/yanshigou/Downloads/16")).scan_with_depth(10).all_file_objects(), '0.0.0.0:8000', "/Users/yanshigou/Downloads/16", datetime.now(), [], "1039,10395,10396")
+    print(b)
