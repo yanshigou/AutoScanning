a56e8a908e685c6d3b4d2504d2a33766ade93fc9
yanshigoudeMacBook <569578851@qq.com>
2022-08-12 02:40:22 +0800

    update

--- wt/wt_main.py
+++ wt/wt_main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = "dzt"
 __date__ = "2022/8/6"
-__title__ = "违停扫描器v1.0_20220807"
+__title__ = "违停扫描器v1.0_20220810"
 
 from wt_file_manager import FileObjectManager, FileObject
 import requests
@@ -12,6 +12,9 @@ from wt_upload_image import event, QZ
 from datetime import datetime
 from time import sleep
 import configparser
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # 建立连接 获取csrftoken
 client = requests.session()
@@ -52,7 +55,7 @@ def basic_info():
 
 
 def event_run(event_path, ip_val, white_list, sleep_time):
-    log_file = datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
+    log_file = BASE_DIR + "/logs/" + datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
     f = open(log_file, 'a+', encoding='utf-8')
     text.insert(tk.END, "开始扫描事件文件夹：%s\n" % event_path)
     f.write("开始扫描事件文件夹：%s\n" % event_path)
@@ -62,7 +65,7 @@ def event_run(event_path, ip_val, white_list, sleep_time):
     # event_bt.config(state="disabled", text='正在扫描事件文件夹')
     count_event_thread_lb.config(text="\n已开启事件扫描器数：%s\n" % str(event_thread_count))
     while True:
-        log_file = datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
+        log_file = BASE_DIR + "/logs/" + datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
         try:
             f = open(log_file, 'a+', encoding='utf-8')
             now_time = datetime.now()
@@ -127,7 +130,7 @@ def event_run(event_path, ip_val, white_list, sleep_time):
 
 
 def QZ_run(qz_path, ip_val, white_list, sleep_time, qz_time, wf_list):
-    log_file = datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
+    log_file = BASE_DIR + "/logs/" + datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
     f = open(log_file, 'a+', encoding='utf-8')
     text.insert(tk.END, "\n开始扫描取证文件夹：%s\n" % qz_path)
     f.write("%s \n开始扫描取证文件夹：%s" % (qz_path, datetime.now()))
@@ -137,7 +140,7 @@ def QZ_run(qz_path, ip_val, white_list, sleep_time, qz_time, wf_list):
     # qz_bt.config(state="disabled", text='正在扫描取证文件夹')
     count_qz_thread_lb.config(text="\n已开启取证扫描器数：%s\n" % str(qz_thread_count))
     while True:
-        log_file = datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
+        log_file = BASE_DIR + "/logs/" + datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
         try:
             f = open(log_file, 'a+', encoding='utf-8')
             now_time = datetime.now()
@@ -209,16 +212,19 @@ def thread_it(func, *args):
 
 
 def auto_run(event_path, ip_val, white_list, sleep_time, qz_time):
-    log_file = datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
+    log_file = BASE_DIR + "/logs/" + datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
     f = open(log_file, 'a+', encoding='utf-8')
     try:
-        text.insert(tk.END, "%s 5秒后自动开始运行 事件扫描 \n" % datetime.now())
-        text.insert(tk.END, "%s 30秒后自动开始运行 取证扫描 \n" % datetime.now())
-        f.write("%s 5秒后自动开始运行 事件扫描 \n" % datetime.now())
-        f.write("%s 30秒后自动开始运行 取证扫描  \n" % datetime.now())
-        sleep(5)
+        text.insert(tk.END, "%s %s秒后自动开始运行 【超速】取证扫描 \n" % (datetime.now(), qz_time))
+        # f.write("%s 5秒后自动开始运行 【超速】取证扫描  \n" % datetime.now())
+
+        sleep(int(qz_time))
+
+        text.insert(tk.END, "%s %s秒后自动开始运行 事件扫描 \n" % (datetime.now(), qz_time))
+        text.insert(tk.END, "%s %s秒后自动开始运行 取证扫描 \n" % (datetime.now(), qz_time))
+        sleep(int(qz_time))
         thread_it(event_run, event_path, ip_val, white_list, sleep_time)
-        sleep(25)
+        sleep(0.1)
         thread_it(QZ_run, qz_path, ip_val, white_list, sleep_time, qz_time, wf_list)
     except Exception as e:
         print(e)
@@ -230,7 +236,7 @@ def auto_run(event_path, ip_val, white_list, sleep_time, qz_time):
 
 if __name__ == '__main__':
 
-    log_file = datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
+    log_file = BASE_DIR + "/logs/" + datetime.now().strftime('%Y-%m-%d') + '违停日志.txt'
     f = open(log_file, 'a+', encoding='utf-8')
     f.write('%s 正在读取配置文件...\n' % datetime.now())
     f.close()
